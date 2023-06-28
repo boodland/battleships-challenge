2f160075769532fe6c4f3ed5ad3b9f940794012c
Boodland <albarlopez@gmail.com>
2023-06-28 18:36:03 +0200

    Add Board tests

--- src/app/board.spec.tsx
+++ src/app/board.spec.tsx
@@ -1,17 +1,39 @@
 import { render } from '@testing-library/react';
 
 import Board from './board';
+import { BoardRowProps } from './board-row';
+
+vi.mock('./utils', () => {
+  return { rows: [0, 1, 2, 3, 4], columns: ['A', 'B', 'C', 'D', 'E'] };
+});
+vi.mock('./board-row', () => ({
+  default: (props: BoardRowProps) =>
+    `MockedBoardRow ${JSON.stringify(props)} onCellClick: ${
+      props.onCellClick.name
+    }`,
+}));
+
+const onShot = (position: number) => null;
 
 describe('Board', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <Board
-        ships={[]}
-        shots={[]}
-        showShips={false}
-        onShot={(position: number) => null}
-      />
+      <Board ships={[]} shots={[]} showShips={false} onShot={onShot} />
     );
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should display columns row', () => {
+    const { container } = render(
+      <Board ships={[]} shots={[]} showShips={false} onShot={onShot} />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should display one BoardRow per row', () => {
+    const { container } = render(
+      <Board ships={[]} shots={[]} showShips={false} onShot={onShot} />
+    );
+    expect(container).toMatchSnapshot();
   });
 });
