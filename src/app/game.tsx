57601845f1e8593e40eb11b0cbe5d3a5689e507d
Boodland <albarlopez@gmail.com>
2023-06-28 13:03:47 +0200

    Add game component

---
+++ src/app/game.tsx
@@ -0,0 +1,52 @@
+import styles from './game.module.css';
+import { useCallback, useState } from 'react';
+import Board from './board';
+import { generateShips, hasGameFinished, initShots } from './utils';
+import GameInfo from './game-info';
+import GameActionsMemoized from './game-actions';
+
+function Game() {
+  const [ships, setShips] = useState(generateShips());
+  const [shots, setShots] = useState(initShots());
+  const [showShips, setShowShips] = useState(false);
+
+  const restartGame = useCallback(() => {
+    setShips(generateShips());
+    setShots(initShots);
+  }, []);
+
+  const toggleShowShips = useCallback(() => {
+    setShowShips((previousValue) => !previousValue);
+  }, []);
+
+  function onShot(position: number) {
+    const newShots = [...shots];
+    newShots[position] = true;
+    setShots(newShots);
+  }
+
+  const hasFinished = hasGameFinished(ships, shots);
+
+  return (
+    <div className={styles['game']}>
+      <div className={hasFinished ? styles['blocked'] : ''}>
+        <Board
+          shots={shots}
+          ships={ships}
+          showShips={showShips}
+          onShot={onShot}
+        />
+      </div>
+      <div className={styles['right-panel']}>
+        <GameActionsMemoized
+          showShips={showShips}
+          onRestartGame={restartGame}
+          onToggleShowShips={toggleShowShips}
+        />
+        <GameInfo shots={shots} ships={ships} />
+      </div>
+    </div>
+  );
+}
+
+export default Game;
